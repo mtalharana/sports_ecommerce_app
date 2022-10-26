ccecad55ab2456e8f8be4a926a1ca7e2a5298337
Talha Rana <mtalharana@gmail.com>
2022-10-26 12:59:45 +0500

    homeScreen Created

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -8,9 +9,75 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(),
+        child: DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 18, top: 20),
+                  child: Text('Shop',
+                      style: GoogleFonts.inter(
+                        fontSize: 28,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.black,
+                      )),
+                ),
+              ],
+            ),
+            TabBar(
+              indicatorColor: Colors.black,
+              indicatorWeight: 3,
+              indicatorSize: TabBarIndicatorSize.label,
+              isScrollable: true,
+              padding: EdgeInsets.only(
+                right: 0,
+              ),
+              tabs: [
+                Tab(
+                    child: Text('Men',
+                        style: GoogleFonts.inter(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black,
+                        ))),
+                Tab(
+                    child: Text('Women',
+                        style: GoogleFonts.inter(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black,
+                        ))),
+                Tab(
+                    child: Text('Childern',
+                        style: GoogleFonts.inter(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black,
+                        ))),
+              ],
+            ),
+            Expanded(
+              child: TabBarView(
+                children: [
+                  Expanded(
+                    child: Container(
+                      color: Colors.red,
+                      child: Text('Talha'),
+                    ),
+                  ),
+                  Icon(Icons.directions_transit, size: 350),
+                  Icon(Icons.directions_car, size: 350),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     ));
   }
 }
